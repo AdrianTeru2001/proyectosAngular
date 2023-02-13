6acfc91fea20dcfe37b4b0a16b0fe0f0c789ee21
AdrianTeru2001 <adrianteru2001@gmail.com>
2023-02-13 17:17:41 +0100

    Modificaciones en los proyectos 001 y 002

--- proyecto001/src/app/app.component.ts
+++ proyecto001/src/app/app.component.ts
@@ -7,4 +7,35 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'proyecto001';
+
+  nombre:string = 'Rodriguez Pablo';
+  edad:number = 40;
+  email:string = 'rpablo@gmail.com';
+  sueldos:number[] = [1700, 1600, 1900];
+  activo:boolean = true;
+
+  esActivo():string {
+    if (this.activo)
+      return 'Trabajador Activo';
+    else
+      return 'Trabajador Inactivo';
+  }
+
+  ultimos3Sueldos() {
+    let suma=0;
+    for(let x=0; x<this.sueldos.length; x++)
+      suma+=this.sueldos[x];
+    return suma;
+  }
+
+  contador:number = 1;
+
+  incrementar():void{
+    this.contador++;
+  }
+
+  decrementar():void{
+    this.contador--;
+  }
+  
 }
