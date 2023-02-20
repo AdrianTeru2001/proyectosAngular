cbaccfdbe9b95acb466ba241ff7b57fbb965b707
AdrianTeru2001 <adrianteru2001@gmail.com>
2023-02-20 20:35:00 +0100

    Relizados ejercicios proyectos 013 y 016

--- proyecto013/src/app/app.component.ts
+++ proyecto013/src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import  { ArticulosService } from './articulos.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'proyecto013';
+
+  articulos: any;
+
+  constructor(private articulosService: ArticulosService) {}
+
+  ngOnInit() {
+    this.articulosService.retornar()
+    .subscribe( result =>  this.articulos = result)
+  }
+
 }
